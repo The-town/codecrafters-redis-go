df548d9720725dc85e54042480f0700d7e5e6e3b
Matsukura <heuchannsight@yahoo.co.jp>
2023-02-02 19:08:40 +0900

    3st

--- app/server.go
+++ app/server.go
@@ -22,13 +22,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 
+	for {
 		data := make([]byte, 1024)
 		count, err := conn.Read(data)
 		if err != nil {
